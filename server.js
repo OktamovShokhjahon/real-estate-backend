32d84c57377c978b3887eda99750c44c78587555
shohjahon <oktamovshohjahon956@gmail.com>
2025-07-27 22:57:05 +0500

    cors

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const app = express();
 
 // Security middleware
 app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 
 // Rate limiting
 const limiter = rateLimit({
