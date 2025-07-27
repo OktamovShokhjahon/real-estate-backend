d8beac65b45a3a169862a821c18377180456101d
shohjahon <oktamovshohjahon956@gmail.com>
2025-07-27 23:01:45 +0500

    cros fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Security middleware
 app.use(helmet());
-app.use(cors);
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: process.env.FRONTEND_URL || "http://localhost:3000",
